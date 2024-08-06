a8131b68e462967c87cb2e369f5aee6669efcb45
saulluiz <sauloluisoliv@gmail.com>
2024-08-06 11:05:57 -0300

    Organizado

--- Program.cs
+++ Program.cs
@@ -1,9 +1,47 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using implementandoQuickSort;
 
-int[] arr={1,2,3,4,15,6,7,8,9,10,11,12,13,14,5};
+int[] arr ={24,
+ 12,
+ 88,
+ 8,
+ 18,
+ 11,
+ 90,
+ 29,
+ 44,
+ 67,
+ 55,
+ 51,
+ 3,
+ 84,
+ 76,
+ 91,
+ 63,
+ 61,
+ 98,
+ 47,
+ 79,
+ 4,
+ 94,
+ 32,
+ 14,
+ 7,
+ 60,
+ 37,
+ 83,
+ 97};
+
+System.Console.WriteLine("Array:");
+
+foreach (var i in arr)
+{   
+    System.Console.Write(i + ",");
+}
 QuickSort.SortArray(arr);
-System.Console.WriteLine("((---------------))");
-foreach(var i in arr){
-    System.Console.Write(i+",");
+
+System.Console.WriteLine("\nOrdenando...");
+foreach (var i in arr)
+{
+    System.Console.Write(i + ",");
 }
